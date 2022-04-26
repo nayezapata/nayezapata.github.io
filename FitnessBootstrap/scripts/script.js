6d99303a0b2311f8f5ad1fc491406550d48dbe9e
nayezapata <102701740+nayezapata@users.noreply.github.com>
2022-04-25 20:26:06 -0600

    Add fitness RWS with Bootstraps.

---
+++ FitnessBootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:Nayeli Zapata Reyes.
+    File Name: script.js
+    Date: April 25, 2022.
+*/
+
+
+$(document) .ready(function(){
+    $('.hero') .height($(window) .height());
+});
